e8020fcab3ffb192faf228ef78dfd86ff59c6681
Shivansh-87 <shivanshh045@gmail.com>
2026-04-08 23:50:47 +0530

    practice here rest on gfg and leetcode

---
+++ Day_27/butterfly.java
@@ -0,0 +1,26 @@
+package Day_27;
+
+public class butterfly{
+    public static void main(String[] args) {
+        int n=5;
+        int spaces=2*n-2;
+        for(int i=1;i<=2*n-1;i++){
+            int star=i;
+            if(i>n) star=2*n-i;
+            for(int j=1;j<=star;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=spaces;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=star;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+            if(i<n) spaces -=2;
+            else spaces +=2;
+        }
+
+    }
+}
+
---
+++ Day_27/countno.java
@@ -0,0 +1,10 @@
+
+package Day_27;
+import java.lang.Math;
+public class countno {
+    public static void main(String[] args) {
+        int n=7789;
+        int cnt=(int) (Math.log10(n)+1);
+        System.out.println(cnt);
+    }
+}
