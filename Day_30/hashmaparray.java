6227eba1328ce3b12d31c853680fbde2fb7b2e80
Shivansh-87 <shivanshh045@gmail.com>
2026-04-11 22:08:36 +0530

    hashing

---
+++ Day_30/hashmaparray.java
@@ -0,0 +1,28 @@
+package Day_30;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class hashmaparray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+        int[] arr =new int[n];
+
+        for (int i=0;i<n;i++) {
+            arr[i]=sc.nextInt();
+        }
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            map.put(arr[i],map.getOrDefault(arr[i], 0)+1);
+        }
+        int q=sc.nextInt();
+
+        while(q-->0){
+            int number=sc.nextInt();
+            System.out.println(map.getOrDefault(number, 0));
+        }
+    }
+}
